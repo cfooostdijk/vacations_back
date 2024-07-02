eb4b8264c8e065f286ebc21459a9890d87f1fbea
CrisOos <cristian.oostdijk@avenida.com>
2024-07-01 21:59:43 -0300

    cors updated

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV['ALLOWED_ORIGINS'] || 'http://localhost:3001'
+    origins 'http://localhost:3001',
+            'https://vacationsfront.netlify.app/'
 
     resource "*",
       headers: :any,
